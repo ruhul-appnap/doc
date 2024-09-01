9930e52ad87b9b918fa61b9a0ff4c4882c493dc6
Ruhul <ruhul@appnap.io>
2024-09-01 11:58:40 +0600

    set up input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
 
 export default function Home() {
+  const [content, setContent] = useState("dsfsfsd");
+
   return (
-    <main
-      className=" border border-green-600 min-h-80 m-4"
-      contentEditable
-    ></main>
+    <textarea
+      className="border border-green-600 min-h-80 m-4 w-[90%]"
+      value={content}
+      onChange={(e) => setContent(e.target.value)}
+    ></textarea>
   );
 }
