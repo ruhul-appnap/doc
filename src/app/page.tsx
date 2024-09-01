207f51c8c0d3dea54c1af3febf40eca3843f8e6a
Ruhul <ruhul@appnap.io>
2024-09-01 13:06:55 +0600

    connect socket

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,53 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { socket } from "./socket";
 
 export default function Home() {
   const [content, setContent] = useState("dsfsfsd");
+  const [isConnected, setIsConnected] = useState(false);
+  const [transport, setTransport] = useState("N/A");
+
+  useEffect(() => {
+    if (socket.connected) {
+      onConnect();
+    }
+
+    function onConnect() {
+      setIsConnected(true);
+      setTransport(socket.io.engine.transport.name);
+
+      socket.io.engine.on("upgrade", (transport) => {
+        setTransport(transport.name);
+      });
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+      setTransport("N/A");
+    }
+
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+
+    return () => {
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+    };
+  }, []);
 
   return (
-    <textarea
-      className="border border-green-600 min-h-80 m-4 w-[90%]"
-      value={content}
-      onChange={(e) => setContent(e.target.value)}
-    ></textarea>
+    <main>
+      <div>
+        <p>Status: {isConnected ? "connected" : "disconnected"}</p>
+        <p>Transport: {transport}</p>
+      </div>
+
+      <textarea
+        className="border border-green-600 min-h-80 m-4 w-[90%]"
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+      ></textarea>
+    </main>
   );
 }
